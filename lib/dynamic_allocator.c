c659ac91e1eee3d0f1ad9cf3b72d73f29e910f4b
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2024-10-31 01:37:53 +0300

    MS2 files updates

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -415,6 +415,7 @@ void *realloc_block_FF(void* va, uint32 new_size)
     if (va == NULL && new_size == 0)
     {
         alloc_block_FF(0);
+        cprintf("VA NULL & SIZE = 0\n");
         return NULL;
     }
 
@@ -422,12 +423,14 @@ void *realloc_block_FF(void* va, uint32 new_size)
     if (new_size == 0)
     {
         free_block(va);
+        cprintf("SIZE = 0\n");
         return NULL;
     }
 
 
     if (va == NULL)
     {
+    	cprintf("VA NULL\n");
         return (void*)alloc_block_FF(new_size);
     }
 
@@ -441,7 +444,7 @@ void *realloc_block_FF(void* va, uint32 new_size)
     {
     	uint32 remainingSize = sizeOfCurrBlock - new_size;
     	forsplitting(va, new_size, sizeOfCurrBlock,remainingSize);
-
+    	//cprintf("NEW size less than or equal size\n");
         return (void*)va;
     }
     if (new_size > sizeOfCurrBlock)
@@ -473,24 +476,28 @@ void *realloc_block_FF(void* va, uint32 new_size)
 
                 LIST_INSERT_HEAD(&freeBlocksList, (struct BlockElement*)newFreeBlock);
             }
-
+            //cprintf("NEW size > size and next is free\n");
             return (void*)va;
         }
         else if(isafterBlockFree == 0 && (sizeOfCurrBlock + sizeOfafterBlock >= new_size))
         {
         	void* ret = alloc_block_FF(new_size);
+        	free_block(va);
         	va=ret;
+        	//NOT HANDLED IN TESTS
+        	cprintf("NEW size > size and next is not free\n");
         	return (void*)ret;
         }
         else
         {
         	void* ret = sbrk(new_size);
         	set_block_data(ret,new_size,1);
+        	cprintf("SBRK\n");
         	return (void*)ret;
         }
     }
 
-
+    cprintf("Nothing\n");
     return NULL;
 }
 
